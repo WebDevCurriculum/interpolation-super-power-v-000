82acb9da618e99cb3941bbb1e0c362848e47761d
Charity Youngblood <youngblood.charity@gmail.com>
2017-10-04 19:45:39 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -7,7 +7,7 @@ $colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 $i = 0
 def display_rainbow(colors)
     $colors.each do |capital_colors|
-     puts "#{$colors[$i].chars.first.capitalize}: #{$colors[$i]}, "
+     print "#{$colors[$i].chars.first.capitalize}: #{$colors[$i]}, "
         $i+=1
     end
 end
