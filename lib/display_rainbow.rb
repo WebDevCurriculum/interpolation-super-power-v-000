f549dd7dc2ca5119b7127fbe877c9a99b876c71e
Charity Youngblood <youngblood.charity@gmail.com>
2017-10-04 19:50:19 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -15,4 +15,5 @@ end
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 def display_rainbow(colors)
   puts "R: #{colors[0]}, O: #{colors[1]}, "
-end 
+end
+display_rainbow(colors)
